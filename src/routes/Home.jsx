a7f34acdc56f9886a0b5cf4c06f1f434fe40b792
rozer4heros <rozer4heros@gmail.com>
2026-07-09 15:25:44 +0900

    feat: refactor Home component to use MUI components for layout and styling

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,32 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Divider from "@mui/material/Divider";
+
 function Home({}) {
   return (
     <>
-      <h2>Home</h2>
+      <Typography variant="h2" component="h2">
+        Home
+      </Typography>
+      <Box component="form" sx={{ m: 2 }} method="post">
+        <TextField
+          sx={{ my: 1 }}
+          fullWidth
+          label="Comment"
+          placeholder="글을 입력해주세요"
+          type="text"
+          name="comment"
+          variant="outlined"
+          multiline
+          rows={5}
+        />
+        <Button sx={{ m: 1 }} type="submit" variant="contained">
+          작성
+        </Button>
+      </Box>
+      <Divider sx={{ my: 3 }} />
     </>
   );
 }
