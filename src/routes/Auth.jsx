276d145120c2d9b596b7b5aada6d5d2b00c3097f
rozer4heros <rozer4heros@gmail.com>
2026-07-09 11:07:43 +0900

    feat: update Auth component with login form and state management

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,7 +1,58 @@
+import { useState } from "react";
+
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
 function Auth({}) {
+  const [newAccount, setNewAccount] = useState(true);
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((p) => ({ ...p, [name]: value }));
+  };
+
+  console.log(form);
+
   return (
     <>
-      <h2>회원가입</h2>
+      <Typography variant="h2" component="h2">
+        Login Form
+      </Typography>
+      <Box
+        component="form"
+        sx={{ m: 2 }}
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <TextField
+          sx={{ mt: 2 }}
+          fullWidth
+          label="Email address"
+          type="email"
+          name="email"
+          variant="outlined"
+          onChange={handleChange}
+        />
+        <TextField
+          sx={{ mt: 2 }}
+          fullWidth
+          label="Password"
+          type="password"
+          name="password"
+          variant="outlined"
+          onChange={handleChange}
+        />
+        <Button sx={{ mt: 2 }} type="submit" variant="contained">
+          Login
+        </Button>
+      </Box>
     </>
   );
 }
