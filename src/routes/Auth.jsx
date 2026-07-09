2fd0cea8492a5d5e223ca0e9549673f97d049682
rozer4heros <rozer4heros@gmail.com>
2026-07-09 13:31:19 +0900

    feat: add Google sign-in functionality to Auth component

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
 import { authService } from "../firebase.js";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
 
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
@@ -16,6 +21,7 @@ function Auth({}) {
     password: "",
   });
   const auth = authService;
+  const googleProvider = new GoogleAuthProvider();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -31,7 +37,7 @@ function Auth({}) {
         .then((userCredential) => {
           // Signed up
           const user = userCredential.user;
-          console.log(userCredential);
+          console.log(user);
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -44,7 +50,7 @@ function Auth({}) {
         .then((userCredential) => {
           // Signed in
           const user = userCredential.user;
-          console.log(userCredential);
+          console.log(user);
         })
         .catch((error) => {
           const errorCode = error.code;
@@ -52,6 +58,32 @@ function Auth({}) {
           console.warn(errorCode, errorMessage);
         });
     }
+  };
+  const onGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+
+        // The signed-in user info.
+        const user = result.user;
+
+        console.log(credential, user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        // The email of the user's account used.
+        const email = error.customData.email;
+
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+
+        console.log(errorCode, errorMessage, email, credential);
+      });
   };
 
   return (
@@ -61,7 +93,7 @@ function Auth({}) {
       </Typography>
       <Box component="form" sx={{ m: 2 }} method="post" onSubmit={onSubmit}>
         <TextField
-          sx={{ mt: 2 }}
+          sx={{ my: 1 }}
           fullWidth
           label="Email address"
           type="email"
@@ -70,7 +102,7 @@ function Auth({}) {
           onChange={handleChange}
         />
         <TextField
-          sx={{ mt: 2 }}
+          sx={{ my: 1 }}
           fullWidth
           label="Password"
           type="password"
@@ -78,12 +110,22 @@ function Auth({}) {
           variant="outlined"
           onChange={handleChange}
         />
-        <Button sx={{ mt: 2 }} type="submit" variant="contained">
+        <Button sx={{ m: 1 }} type="submit" variant="contained">
           {newAccount ? "회원가입" : "로그인"}
+        </Button>
+        <Divider sx={{ my: 1 }} />
+        <Button sx={{ m: 1 }} type="button" variant="contained" onClick={onGoogleSignIn}>
+          {newAccount ? "구글로 회원가입" : "구글로 로그인"}
         </Button>
       </Box>
       <Divider sx={{ my: 3 }} />
-      <Button sx={{ mt: 2 }} type="button" variant="contained" onClick={() => setNewAccount((p) => !p)}>
+      <Button
+        sx={{ my: 2 }}
+        color="secondary"
+        type="button"
+        variant="contained"
+        onClick={() => setNewAccount((p) => !p)}
+      >
         {newAccount ? "로그인으로 전환" : "회원가입으로 전환"}
       </Button>
     </>
