8033170805177cf3be68ebe5d7e9b6e2cd9b3e6d
rozer4heros <rozer4heros@gmail.com>
2026-07-09 16:09:15 +0900

    feat: implement comment submission functionality in Home component

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+
+import { db } from "../firebase";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
@@ -5,12 +10,28 @@ import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
 
 function Home({}) {
+  const [comment, setComment] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const docRef = await addDoc(collection(db, "comments"), {
+        // comment: comment,
+        comment,
+        date: serverTimestamp(),
+      });
+      setComment("");
+    } catch (e) {
+      console.error("글 등록 중 오류 발생:", e);
+    }
+  };
+
   return (
     <>
       <Typography variant="h2" component="h2">
         Home
       </Typography>
-      <Box component="form" sx={{ m: 2 }} method="post">
+      <Box component="form" sx={{ m: 2 }} method="post" onSubmit={onSubmit}>
         <TextField
           sx={{ my: 1 }}
           fullWidth
@@ -21,9 +42,11 @@ function Home({}) {
           variant="outlined"
           multiline
           rows={5}
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
         />
         <Button sx={{ m: 1 }} type="submit" variant="contained">
-          작성
+          등록
         </Button>
       </Box>
       <Divider sx={{ my: 3 }} />
